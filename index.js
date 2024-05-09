103d16b40de4de1069a32d357c2ea86e9421c828
onorris <oliviagdewald@gmail.com>
2024-05-09 17:14:58 -0500

    creating new Express server

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const express = require('express');
 
 //declares 'app' variable that encapsulates Express's functionality to configure your web server//
 const app = express();
+const port = 8080
 
+//displays text//
 app.get('/', (req, res) => {
     res.send('Welcome to my Movie Club!');
 })
 
+//pulls movie data//
 app.get('/movies', (req, res) => {
     res.json(topTenMovies);
 })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
