a8b90b499bf9ed09630d24c6e4f107c24db61bf8
onorris <oliviagdewald@gmail.com>
2024-05-09 17:00:02 -0500

    express routing for text response and json object

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+//imports the express module locally so it can be used //
 const express = require('express');
+
+//declares 'app' variable that encapsulates Express's functionality to configure your web server//
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my Movie Club!');
+})
+
+app.get('/movies', (req, res) => {
+    res.json(topTenMovies);
+})
