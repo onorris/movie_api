613782bf5971b118306bd2f49b308487116d18c7
onorris <oliviagdewald@gmail.com>
2024-06-14 13:05:33 -0500

    Update to user code to return JSON info re: all users and register new user

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const express = require('express');
 
 //declares 'app' variable that encapsulates Express's functionality to configure your web server//
 const app = express();
+app.use(express.json()),
+app.use(express.urlencoded({extended: true }));
 
 //uuid (universally unique identifier) module//
 const uuid = require('uuid');
@@ -25,19 +27,13 @@ const { title } = require('process');
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 const port = 8080
 
-//express.static serves documentation.html file from the public folder//
-app.use(express.static(path.join(__dirname, 'public')));
 
-//setup the Middleware//
-app.use(morgan('combined', {stream:accessLogStream}));
-
-
-//GET requests//
+//default text response//
 app.get('/', (req, res) => {
     res.send('Welcome to my Movie Club!');
 })
 
-//returns JSON object with list of all movies//
+//returns JSON object with list of all movies when at /movies //
 app.get('/movies', (req, res) => {
     Movies.find()
     .then((movies) => {
@@ -85,6 +81,79 @@ app.get('/movies/directors/:directorName', async (req, res) => {
     });
 });
 
+//Returns JSON object of all users //
+app.get("/users", function (req, res) {
+    Users.find()
+    .then(function (users) {
+        res.status(201).json(users);
+    })
+    .catch(function(err) {
+        console.error(err);
+        res.status(500).send("Error: "+ err);
+    });
+});
+
+//Allows new users to register//
+/* JSON in this format:
+{ID: integer
+Username: String,
+Password, String,
+Email: String,
+Birthday: Date
+}*/
+app.post("/users", async (req, res) => {
+    await Users.findOne({Username: req.body.Username})
+    .then((user) => {
+        if (user) {
+            return res.status(400).send((req.body.Username) + "already exists");
+        } else {
+            Users.create({
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday,
+            })
+            .then((user) => {
+                res.status(201).json(user);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send("Error: " + err);
+            })
+        }
+        })
+    .catch((error) => {
+        console.log(error);
+        res.status(500).send("Error: "+ err);
+    });
+});
+
+//Allow new users to update their user info (username)//
+/* JSON in this format:
+{Username: String, (required)
+Password: String, (required)
+Email: String, (required)
+Birthday: Date}*/
+//firt updates users with a certain username, then uses $set to specify which fields in the user document you're updating//
+app.put('/users/:Username', async (req, res) => {
+    await Users.findOneAndUpdate( {Username: req.params.Username},
+        {$set:
+            {Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+            }
+        },
+       {new: true}) //This line makes sure that the updated document is returned//
+    .then((updatedUser) => {
+        res.json(updatedUser);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+    }) 
+});
+
 //Allow users to add a movie to their list of favorites//
 app.put ('/users/id/:favorites', (req, res) => {
     res.send('Successful PUT request adding movie to Favorites List')
@@ -95,20 +164,16 @@ app.delete('/users/id/:favorites', (req, res) => {
     res.send('Successful DELETE request removing movie from Favorites List')
 });
 
-//Allows new users to register//
-app.post('/users/id/:register', (req, res) => {
-    res.send('Successful POST request adding user to site')
-});
-
-//Allow new users to update their user info (username)//
-app.put('/users/:id', (req, res) => {
-    res.send('Successful PUT request indicating that username is updated')
-});
-
 //Allow existing users to deregister (showing text that a user email has been removed)//
 app.delete('/users/:id', (req, res) => {
     res.send('Successful DELETE request removing user from website')
 });
+
+//express.static serves documentation.html file from the public folder//
+app.use(express.static(path.join(__dirname, 'public')));
+
+//setup the Middleware//
+app.use(morgan('combined', {stream:accessLogStream}));
 
 //listen for requests
 app.listen(port, () => {
